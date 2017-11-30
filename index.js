f976c8c026b9c91b3183b0a1dd3eb910d7dafe88
jasonjzgithub <32497678+jasonjzgithub@users.noreply.github.com>
2017-11-30 14:17:34 -0500

    Bootstrap index.js and package.json

---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports = {
+    escape: function(html) {
+      return String(html)
+        .replace(/&/g, '&amp;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#39;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;');
+    }
+};
